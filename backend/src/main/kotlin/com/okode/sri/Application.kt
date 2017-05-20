e603a5c1c11921e6454ad455e0cb8c6bc2b6dab3
Pedro Jorquera <pjorquera@okode.com>
2017-05-20 20:46:08 +0200

    Refactor Java -> Kotlin

---
+++ backend/src/main/kotlin/com/okode/sri/Application.kt
@@ -0,0 +1,35 @@
+package com.okode.sri
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+import java.time.Duration
+import java.time.Instant
+
+@SpringBootApplication
+@RestController
+@CrossOrigin
+class Application {
+
+    @GetMapping(
+            path = arrayOf("/date/now/{durationSeconds}"),
+            produces = arrayOf(
+                    "text/event-stream",
+                    "application/stream+json",
+                    "application/json"))
+    fun now(@PathVariable durationSeconds: Int): Flux<Instant> {
+        return Flux
+                .interval(Duration.ofMillis(30))
+                .map { i -> Instant.now() }
+                .take(Duration.ofSeconds(durationSeconds.toLong()))
+    }
+
+}
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
